195862f3602a038cfe415e255a4a1e58c80c7a9b
ENFSIN <ivanderivan@gmail.com>
2024-06-22 17:26:44 +0700

    Home work 4

---
+++ Projects/Peratori oynoshenii/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    /// <summary>
+    /// Проверка числа на четность.
+    /// </summary>
+   public class Program
+    {
+        /// <summary>
+        /// Вызов програмы.
+        /// </summary>
+        /// <param name="args">Аргументы параметров.</param>
+       public static void Main(string[] args)
+       {
+            int a;
+            a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Число четное.");
+            }
+            else
+            {
+                Console.WriteLine("Число не четное.");
+            }
+       }
+   }
+}
