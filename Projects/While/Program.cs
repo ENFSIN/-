c509c4c8bbb351b3c65d935ac5f265d6a67fe4c1
ENFSIN <ivanderivan@gmail.com>
2024-06-29 17:12:17 +0700

    New

--- Projects/While/Program.cs
+++ Projects/While/Program.cs
@@ -9,23 +9,19 @@
         /// Вызов програмы.
         /// </summary>
         /// <param name="args">Аргументы параметров.</param>
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
             uint oddNumbersCount = 0;
-
             uint evenNumbersCount = 0;
-
             int oddNumbersSum = 0;
-
             int evenNumbersSum = 0;
-
             Console.WriteLine("Введите первое число диапазона");
             int currentValue = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Введите последнее число диапазона");
             int limit = int.Parse(Console.ReadLine());
 
-            while (currentValue <= limit) 
+            while (currentValue <= limit)
             {
                 if (currentValue % 2 == 0)
                 {
@@ -39,13 +35,11 @@
                 }
                 currentValue++;
             }
-
             Console.WriteLine($"Количестиво нечетных чисел:{oddNumbersCount}");
             Console.WriteLine($"Колличество четных чисел:{evenNumbersCount}");
             Console.WriteLine($"Сумма нечетных чисел:{oddNumbersSum}");
             Console.WriteLine($"Сумма четных чисел:{evenNumbersSum}");
             Console.ReadLine();
-
         }
     }
 }
