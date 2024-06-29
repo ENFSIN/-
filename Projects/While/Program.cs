cf22e52c668d300b4af14010e6f370f84e3eaa35
ENFSIN <ivanderivan@gmail.com>
2024-06-29 16:26:08 +0700

    Sycle while

---
+++ Projects/While/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace While
+{
+    /// <summary>
+    /// Цикл while.
+    /// </summary>
+   public class Program
+    {
+        /// <summary>
+        /// Вызов програмы.
+        /// </summary>
+        /// <param name="args">Аргументы параметров.</param>
+        public static void Main(string[] args) 
+        {
+            uint oddNumbersCount = 0;
+
+            uint evenNumbersCount = 0;
+
+            int oddNumbersSum = 0;
+
+            int evenNumbersSum = 0;
+
+            Console.WriteLine("Введите первое число диапазона");
+            int currentValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите последнее число диапазона");
+            int limit = int.Parse(Console.ReadLine());
+
+            while (currentValue <= limit) 
+            {
+                if (currentValue % 2 == 0)
+                {
+                    evenNumbersCount++;
+                    evenNumbersSum += currentValue;
+                }
+                else
+                {
+                    oddNumbersCount++;
+                    oddNumbersSum += currentValue;
+                }
+                currentValue++;
+            }
+
+            Console.WriteLine($"Количестиво нечетных чисел:{oddNumbersCount}");
+            Console.WriteLine($"Колличество четных чисел:{evenNumbersCount}");
+            Console.WriteLine($"Сумма нечетных чисел:{oddNumbersSum}");
+            Console.WriteLine($"Сумма четных чисел:{evenNumbersSum}");
+            Console.ReadLine();
+
+        }
+    }
+}
