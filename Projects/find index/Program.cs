d9bca9e1cb4d0af25e600c2e63ca3a5ae5359be1
ENFSIN <ivanderivan@gmail.com>
2024-08-07 12:22:47 +0700

    Ty

---
+++ Projects/find index/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace lessons
+{
+   class Program
+   { 
+        static  int Indexof(int[] array, int value)
+        { 
+            for (int i = 0; i < array.Length; i++) 
+            { 
+                if (array[i] == value)
+                { 
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] myarray = { 2, -5, 23, 55 };
+
+            int result = Indexof(myarray, 23);
+            
+            Console.WriteLine(result);
+        }
+   }
+}
