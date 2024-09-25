08eb4ba156d5c98f1ba2e1a0a081fc56fc4497e6
ENFSIN <ivanderivan@gmail.com>
2024-09-25 22:02:09 +0700

    Agane

---
+++ Projects/resize/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace resize
+{
+    class programm
+    {
+        public static void Resize(ref int[] array, int newSize)
+        {
+            int[] newarray = new int[newSize];
+            for (int i = 0; i < array.Length && i< newarray.Length; i++)
+            {
+                newarray[i] = array[i];
+            }
+
+            array = newarray;
+        }
+
+        public static void main(String[] args)
+        {
+            int[] mainarray = { 1, 2, 3, 4 };
+            programm.Resize(ref mainarray, 10);
+        }
+    }
+}
